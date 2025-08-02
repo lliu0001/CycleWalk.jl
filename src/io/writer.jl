1683a0c406158a8b259b4885be65420457809d02
Lauren Liu <lauren.liu@duke.edu>
2025-08-02 17:22:08 -0400

    Changed project directory

--- src/io/writer.jl
+++ src/io/writer.jl
@@ -37,7 +37,7 @@ function Writer(
     end
 
     f = @__FILE__
-    projdir = "/"*joinpath(split(f, "/")[1:end-1])
+    projdir = "/Users/lliu001/Desktop/CycleWalk.jl"
     commit = split(read(`git -C $projdir log`, String), "\n")[1]
     commit = replace(commit, "commit "=>"")
     repository = read(`git -C $projdir config --get remote.origin.url`, String)
