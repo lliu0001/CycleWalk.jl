c7823e0f34fb4d9b316755b4ef5c19d2dc32755e
Lauren Liu <lauren.liu@duke.edu>
2025-08-02 18:41:25 -0400

    Official

--- examples/runCycleWalk_ct.jl
+++ examples/runCycleWalk_ct.jl
@@ -3,7 +3,7 @@
 
 ## Activate the CycleWalk environment and load necessary packages
 import Pkg
-Pkg.activate("Users/lliu001/Desktop/CycleWalk") #Changed from Pkg.activate("CycleWalk_env"; shared=true)
+Pkg.activate("/Users/lliu001/Desktop/CycleWalk") #Changed from Pkg.activate("CycleWalk_env"; shared=true)
 
 using RandomNumbers
 using CycleWalk
